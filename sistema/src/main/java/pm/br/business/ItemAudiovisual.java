5c477374ce8bf7dbfc0be5bd62a4b158fb5b2d96
VitorCostaVianna <vcvianna@sga.pucminas.br>
2024-11-12 11:30:04 -0300

    add calcularCusto, toString, setters - itemAudioVisual

--- sistema/src/main/java/pm/br/business/ItemAudiovisual.java
+++ sistema/src/main/java/pm/br/business/ItemAudiovisual.java
@@ -2,14 +2,42 @@ package pm.br.business;
 
 public class ItemAudiovisual extends ItemServico {
     private String equipamento;
+    private Double precoUnitario;
 
-    public ItemAudiovisual(String tipo, float custo, int quantidade, String descricao, String equipamento) {
+    public ItemAudiovisual(String tipo, Double custo, int quantidade, String descricao, String equipamento, Double precoUnitario) {
         super(tipo, custo, quantidade, descricao);
-        this.equipamento = equipamento;
+        this.setEquipamento(equipamento);
+        this.setPrecoUnitario(precoUnitario);
     }
 
     public String getEquipamento() {
         return equipamento;
+    }
+
+    public void setEquipamento(String equipamento) {
+        this.equipamento = equipamento;
+    }
+
+    public Double getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setPrecoUnitario(Double precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(super.toString());
+        sb.append("Equipamento: ").append(this.getEquipamento()).append("\n");
+        sb.append("Preco Unitario: ").append(this.getPrecoUnitario()).append("\n");
+        return sb.toString();
+    }
+    
+    @Override
+    public Double calcularCustoTotal(){
+        return this.getPrecoUnitario() * this.getQuantidade();
     }
 
 }
