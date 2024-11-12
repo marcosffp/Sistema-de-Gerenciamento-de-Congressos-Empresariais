388e4b9945b1e8299aeaa22114c29995d5665611
VitorCostaVianna <vcvianna@sga.pucminas.br>
2024-11-12 11:55:58 -0300

    add cnpj no fornecedor

--- sistema/src/main/java/pm/br/business/Fornecedor.java
+++ sistema/src/main/java/pm/br/business/Fornecedor.java
@@ -2,26 +2,36 @@ package pm.br.business;
 
 public class Fornecedor extends Pessoa {
 
-    private String tipoServico;
+  private String tipoServico;
+  private String cnpj;
 
-    public Fornecedor(String nome, String tipoServico) {
-        super(nome);
-       this.setTipoServico(tipoServico);
-    }
+  public Fornecedor(String nome, String tipoServico, String cnpj) {
+    super(nome);
+    this.setTipoServico(tipoServico);
+    this.setCnpj(cnpj);
+  }
 
-    public String getTipoServico() {
-        return tipoServico;
-    }
+  public String getTipoServico() {
+    return tipoServico;
+  }
 
-    public void setTipoServico(String tipoServico) {
-        this.tipoServico = tipoServico;
-    }
+  public void setTipoServico(String tipoServico) {
+    this.tipoServico = tipoServico;
+  }
 
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(super.toString());
-        sb.append("Tipo de Servico: ").append(this.getTipoServico()).append("\n");
-        return sb.toString();
-    }
+  public String getCnpj() {
+    return cnpj;
+  }
+
+  public void setCnpj(String cnpj) {
+    this.cnpj = cnpj;
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append(super.toString());
+    sb.append("Tipo de Servico: ").append(this.getTipoServico()).append("\n");
+    return sb.toString();
+  }
 }
