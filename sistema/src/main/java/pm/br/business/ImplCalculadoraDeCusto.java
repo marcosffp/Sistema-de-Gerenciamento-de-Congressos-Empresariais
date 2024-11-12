ec1a0f2380b8e1442b038c88a78a48f8cf45f158
VitorCostaVianna <vcvianna@sga.pucminas.br>
2024-11-12 11:29:20 -0300

    mudança metodo calcularCusto para stream

--- sistema/src/main/java/pm/br/business/ImplCalculadoraDeCusto.java
+++ sistema/src/main/java/pm/br/business/ImplCalculadoraDeCusto.java
@@ -6,11 +6,8 @@ public class ImplCalculadoraDeCusto implements CalculadoraDeCusto {
 
     @Override
     public float calcularCusto(List<ItemServico> itemServicos) {
-        float total = 0;
-        for (ItemServico item : itemServicos) {
-            total += item.calcularCustoTotal();
-        }
-        return total;
+       return (float) itemServicos.stream()
+       .mapToDouble(ItemServico::calcularCustoTotal)
+       .reduce(0, (a, b) -> a + b);
     }
-
 }
